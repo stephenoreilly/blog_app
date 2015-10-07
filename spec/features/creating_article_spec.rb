4b86f3e1679ee029702acd85cb0e399fd32bb109
Stephen O'Reilly <stiofan.oreilly@gmail.com>
2015-10-07 14:53:41 +0000

    Link articles to Users

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.feature "Creating Articles" do
+  before do
+    @john = User.create!(email: "john@example.com", password: "password")
+    login_as(@john)
+  end
   scenario "A user creaes a new article" do
     visit "/"
 
@@ -12,6 +16,7 @@ RSpec.feature "Creating Articles" do
 
     expect(page).to have_content ("Article has been created")
     expect(page.current_path).to eq(articles_path)
+    expect(page).to have_content("Created by: #{@john.email}")
 
   end
   scenario "A user fails to create a new article" do
@@ -22,10 +27,10 @@ RSpec.feature "Creating Articles" do
     fill_in "Title", with: ""
     fill_in "Body", with: ""
     click_button "Create Article"
-    
+
     expect(page).to have_content ("Article has not been created")
     expect(page).to have_content ("Title can't be blank")
     expect(page).to have_content ("Body can't be blank")
-    
+
   end
 end
